f87a7f20c0ca27fa1061fb32396f79abeefd0b9c
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-10-10 19:53:56 -0500

    added js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log("Hello World!");
+
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+    });
+  };
+
+  //Call all the inner functions
+  startGame();
+  updateScore();
+};
